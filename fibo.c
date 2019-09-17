2aa6032f7e59784583e539198db50522dcab82ba
Nic Volanschi <eugene.volanschi@inria.fr>
2019-09-17 13:42:57 +0200

    corriges de fibo & maxf

--- fibo.c
+++ fibo.c
@@ -1,19 +1,26 @@
-// fibo.c: Fibonacci Series using Dynamic Programming                                    
+// fibo.c: Fibonacci Series using Dynamic Programming
 #include <stdio.h>
 
 int fibo(int n) {
-  int f[n+1];
   int i;
+  int f_n, f_n_1, f_n_2;
 
   if(n < 1)
     return 0;
 
-  f[0] = 0; // Initial values
-  f[1] = 1;
+  f_n_2 = 0; // Initial values
+  f_n_1 = 1;
   for (i = 2; i <= n; i = i + 1) {
-      /* Add the previous 2 numbers in the series                               
+      /* Add the previous 2 numbers in the series
          and store it */
-      f[i] = f[i-1] + f[i-2];
+      f_n = f_n_1 + f_n_2;
+      f_n_2 = f_n_1;
+      f_n_1 = f_n;
     }
-  return f[n];
+  return f_n;
+}
+
+int main(int argc, char **argv) {
+  int n = 10;
+  printf("fibonnaci(%i)=%i\n", n, fibo(n));
 }
--- maxf.c
+++ maxf.c
@@ -9,28 +9,74 @@
 
  */
 
-#include <stdio.h> 
+#include <stdio.h>
 int max(int a, int b) {
-  if (a > b) // Parenthèses OBLIGATOIRES 
-    return a; 
-  else 
-    return b; 
-} 
+  if (a > b) // Parenthèses OBLIGATOIRES
+    return a;
+  else
+    return b;
+}
 
-int maxTableau(int tableau[], 
-	       int nbElements) { 
-  int resultat = tableau[0]; 
-  int i = 1; 
-  while (i < nbElements) { // celles-ci aussi 
-    resultat = max(resultat, tableau[i]); 
-    i = i + 1; 
-  } 
-  return resultat; 
-} 
+int min(int a, int b) {
+  if (a < b) // Parenthèses OBLIGATOIRES
+    return a;
+  else
+    return b;
+}
 
-int main(int argc, char **argv) { 
-  int tab[] = {10, 20, 42, 40}; 
-  int res = maxTableau(tab, 4); 
-  printf("Max du tableau: %i\n", res); 
-  return 0; 
-} 
+int maxTableau(int tableau[],
+	       int nbElements) {
+  int resultat = tableau[0];
+  int i = 1;
+  while (i < nbElements) { // celles-ci aussi
+    resultat = max(resultat, tableau[i]);
+    i = i + 1;
+  }
+  return resultat;
+}
+
+int secondTableau(int tableau[],
+	       int nbElements) {
+  int premier;
+  int second;
+  if (tableau[0] < tableau[1]) {
+      premier = tableau[1];
+      second = tableau[0];
+  } else {
+      premier = tableau[1];
+      second = tableau[0];
+  }
+  for (int i = 2; i < nbElements; i = i + 1) {
+      if (tableau[i] >= premier) {
+          second = premier;
+          premier = tableau[i];
+      } else if (tableau[i] >= second) {
+          second = tableau[i];
+      }
+  }
+  return second;
+}
+
+int minTableau(int tableau[],
+	       int nbElements) {
+  int resultat = tableau[0];
+  int i = 1;
+  while (i < nbElements) { // celles-ci aussi
+    resultat = min(resultat, tableau[i]);
+    i = i + 1;
+  }
+  return resultat;
+}
+
+#define N 6
+
+int main(int argc, char **argv) {
+  int tab[N] = {10, 20, 42, 40, 40, 42};
+  int res = maxTableau(tab, N);
+  printf("Max du tableau: %i\n", res);
+  res = minTableau(tab, N);
+  printf("Min du tableau: %i\n", res);
+  res = secondTableau(tab, N);
+  printf("2nd du tableau: %i\n", res);
+  return 0;
+}
