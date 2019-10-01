56817ce6e5e0f87f934148d988597fc02da2bad9
Nic Volanschi <eugene.volanschi@inria.fr>
2019-10-01 11:04:21 +0200

    rajoute corriges pour puissance2.c & ingredients.c

--- ingredients.c
+++ ingredients.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 #define N 20 // nbre max d’ingrédients différents
 #define SIZE 32 // taille max d’un nom d’ingrédient
 struct ingredient {
@@ -7,9 +10,25 @@ struct ingredient {
 struct ingredient tab[N]; // liste d’ingrédients totalisés
 int n = 0; // nbre d’ingrédients dans tab[]
 
-int read_ingredient(struct ingredient *s);
-void print_ingredient(struct ingredient *s);
-void add_ingredient(struct ingredient *s);
+int read_ingredient(struct ingredient *s) {
+    return scanf("%i", &(s->quant)) == 1 &&
+            scanf("%s", s->name) == 1;
+}
+
+void print_ingredient(struct ingredient *s) {
+    printf("%i %s\n", (*s).quant, s->name);
+}
+
+void add_ingredient(struct ingredient *s) {
+    for (int i = 0; i < n; i++) {
+        if (!strcmp(s->name,  tab[i].name)) {
+            tab[i].quant += s->quant;
+            return;
+        }
+    }
+    tab[n] = *s;
+    n++;
+}
 
 int main(int argc, char *argv[]) {
   struct ingredient ingredient1;
