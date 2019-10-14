77c3699f455950b26fe7f95db5d0210c8479ea52
Nic Volanschi <eugene.volanschi@inria.fr>
2019-10-14 11:30:59 +0200

    corrige partiel de liste.c

--- liste.c
+++ liste.c
@@ -9,7 +9,7 @@ struct ent {
 };
 
 // doit être appelée avec le pointeur de début de la liste  ex : ajout(&deb,56)
-void ajoutdeb (pt * d, int data) { 
+void ajoutdeb (pt * d, int data) {
   pt paux = (pt) malloc(sizeof(struct ent));
   paux->valeur = data;
   paux->suiv = *d;
@@ -17,7 +17,7 @@ void ajoutdeb (pt * d, int data) {
 }
 
 // doit être appelée avec le pointeur de début de la liste ex: detruit (&L,42)
-void supprime (pt * d, int data) { 
+void supprime (pt * d, int data) {
   pt p = *d;
   pt prec = NULL;
   while (p != NULL) {
@@ -26,14 +26,14 @@ void supprime (pt * d, int data) {
       else *d = (*d)->suiv;
       free(p);
       return;
-    } 
+    }
     // on avance dans la liste
     prec = p;
     p = p->suiv;
   }
-} 
+}
 
-void suppr (pt * d, int data) { 
+void suppr (pt * d, int data) {
   while (*d != NULL) {
     if ((*d)->valeur == data) { // on a trouvé l'élément à supprimer
       pt p = *d;
@@ -43,16 +43,39 @@ void suppr (pt * d, int data) {
     }
     d = &(*d)->suiv; // on avance dans la liste
   }
-} 
+}
 
 void affiche(pt deb) {
-  pt paux = deb;  
+  pt paux = deb;
   // on prend un pointeur auxiliaire pour ne pas perdre l'adresse de début de la liste
   while(paux != NULL) {
     printf("%d ", paux->valeur);
     paux = paux->suiv;
   }
   printf("\n");
+}
+
+pt adresse(pt deb, int data) {
+  pt paux = deb;
+  // on prend un pointeur auxiliaire pour ne pas perdre l'adresse de début de la liste
+  while(paux != NULL) {
+     if ( paux->valeur == data) {
+         return paux;
+     }
+     paux = paux->suiv;
+  }
+  return NULL;
+}
+
+void detruit_liste(pt *deb) {
+  pt paux = *deb;
+  // on prend un pointeur auxiliaire pour ne pas perdre l'adresse de début de la liste
+  while(paux != NULL) {
+      pt tmp = paux;
+      free(paux);
+     paux = tmp->suiv;
+  }
+  *deb = NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -63,11 +86,18 @@ int main(int argc, char *argv[]) {
   }
 
   // Afficher la liste
+  affiche(deb);
 
   // Ajouter l'élèment 99 à la fin
 
   // Si l'élément 55 existe signaler un succès & supprimer;
   // sinon signaler un échec
+  if (adresse(deb, 55) != NULL)
+    printf("succes\n");
+    else  printf("echec\n");
+
 
   // Destruction de toute la liste
+  detruit_liste(&deb);
+  affiche(deb);
 }
